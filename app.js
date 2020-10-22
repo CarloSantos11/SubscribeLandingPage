726b5b15b29efc6ba8046261c16c934aad36b82a
Carlo Santos <carlo@snapit.solutions>
2020-10-22 10:23:30 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){  /* this get set off when hitting localhost:3000 '/'*/
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req,res){
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName, 
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us2.api.mailchimp.com/3.0/lists/ce0c5e0ed4";
+  const options = {
+    method: "POST",
+    auth: "Carlo1:d86ae187e64700611a690f93e8864570-us2"
+  };
+
+
+  
+  const request = https.request(url, options, function(response) {
+
+    if(response.statusCode == 200){
+      res.sendFile(__dirname + "/success.html");
+    }else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+});
+
+// API KEY: d86ae187e64700611a690f93e8864570-us2
+// Audience ID: ce0c5e0ed4
